32e5e1bf23302365cfd81bbd476fec3d664a7285
ろむねこ <nem@nem.x0.to>
2024-04-15 15:14:02 +0900

    Deployスクリプト作成

---
+++ deploy.sh
@@ -0,0 +1,61 @@
+#!/opt/homebrew/bin/bash
+
+# semi-automatic deployment script
+
+IS_DEBUG="true"
+
+SOURCE_DIR="/Users/rca/proj/trident/WebApp/source"
+HTDOCS_DIR="/Applications/XAMPP/htdocs"
+
+function logger {
+    if [[ -v IS_DEBUG ]]; then
+        if [ "${IS_DEBUG}" = "false" ]; then
+                exit
+        fi
+    fi
+
+    head=""
+    case $1 in
+        "err_c")
+                head="\e[41m ERROR \e[00m";;
+        "err")
+                head="\e[31m ERROR \e[00m";;
+        "warn")
+                head="\e[33m WARN  \e[00m";;
+        "info")
+                head="\e[34m INFO  \e[00m";;
+        "succ")
+                head="\e[32m SUCC  \e[00m";;
+        *)
+                head="$1"
+    esac
+
+    printf "${head} : ${2}\n"
+}
+
+function _deploy {
+    logger info "Deploying..."
+    \cp -f -r ${SOURCE_DIR}/* ${HTDOCS_DIR}/
+    logger "succ" "Deployment completed"
+}
+
+function _greet {
+    logger "info" "Welcome to the deployment script"
+    logger "info" "This script will deploy the source code to the htdocs directory"
+    logger "info" "Source directory: ${SOURCE_DIR}"
+    logger "info" "Destination directory: ${HTDOCS_DIR}"
+}
+
+# Logic
+continue=1
+
+_greet
+
+while [ $continue -eq 1 ]; do
+    read -p "Press any key then press enter to deploy again or 'q' to quit: " input
+    if [ "$input" = "q" ]; then
+        continue=0
+    else
+        _deploy
+    fi
+done
