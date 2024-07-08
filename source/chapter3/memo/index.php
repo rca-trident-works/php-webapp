d0449dcb3be0faa908430cbd8bce882f01f8111b
ろむねこ <nem@nem.x0.to>
2024-07-08 14:55:26 +0900

    wip

--- source/chapter3/memo/index.php
+++ source/chapter3/memo/index.php
@@ -1,4 +1,13 @@
 <?php require('dbconnect.php'); ?>
 <!DOCTYPE html>
 <html lang="ja">
+    <body>
+        <h2>Practice</h2>
+        <pre>
+            <?php
+                $memos = $db->prepare('SELECT * FROM memos ORDER BY id DESC');
+            ?>
+        </pre>
+    </body>
+</html>
 
