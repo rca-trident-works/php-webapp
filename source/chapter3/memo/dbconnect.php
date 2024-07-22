81df8546971637b379d6aa25740b975e4e0c0984
ろむねこ <nem@nem.x0.to>
2024-07-22 15:13:03 +0900

    fix indent

--- source/chapter3/memo/dbconnect.php
+++ source/chapter3/memo/dbconnect.php
@@ -2,6 +2,5 @@
 try {
     $db = new PDO('mysql:dbname=mydb;host=db;charset=utf8', 'root', 'root');
 } catch (PDOException $e) {
-    print('DB接続エラー：'.$e->getMessage());
+    print('DB接続エラー：' . $e->getMessage());
 }
-?>
