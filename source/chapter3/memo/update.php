dcc8348a23d1df81f88aa754968b39c84c8fcd64
ろむねこ <nem@nem.x0.to>
2024-07-22 15:02:09 +0900

    移植

---
+++ source/chapter3/memo/update.php
@@ -0,0 +1,18 @@
+<?php require('./dbconnect.php'); ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<body>
+    <h2>Practice</h2>
+    <pre>
+            <?php
+            $memos = $db->prepare('SELECT * FROM memos WHERE id=?');
+            $memos->execute(array($_REQUEST['id']));
+            $memo = $memos->fetch();
+            ?>
+        </pre>
+
+    <article>
+        <pre><?php print($memo['memo']); ?></pre>
+    </article>
+</body>
