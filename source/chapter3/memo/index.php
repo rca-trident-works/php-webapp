465bf71173bc9e29b18994feb8b1434f79a0f423
ろむねこ <nem@nem.x0.to>
2024-07-22 15:00:47 +0900

    ページング推移

--- source/chapter3/memo/index.php
+++ source/chapter3/memo/index.php
@@ -4,16 +4,16 @@
 
 <body>
     <h2>Practice</h2>
-        <?php
-        if (isset($_REQUEST['page']) && is_numeric($_REQUEST['page'])) {
-            $page = $_REQUEST['page'];
-        } else {
-            $page = 1;
-        }
-        $start = 5 * ($page - 1);
-        $memos = $db->prepare('SELECT * FROM memos ORDER BY id DESC LIMIT ?,5');
-        $memos->bindParam(1, $start, PDO::PARAM_INT);
-        $memos->execute();
+    <?php
+    if (isset($_REQUEST['page']) && is_numeric($_REQUEST['page'])) {
+        $page = $_REQUEST['page'];
+    } else {
+        $page = 1;
+    }
+    $start = 5 * ($page - 1);
+    $memos = $db->prepare('SELECT * FROM memos ORDER BY id DESC LIMIT ?,5');
+    $memos->bindParam(1, $start, PDO::PARAM_INT);
+    $memos->execute();
     ?>
     <article>
         <?php while ($memo = $memos->fetch()): ?>
@@ -21,6 +21,19 @@
             <time><?php print($memo['created_at']); ?></time>
             <hr>
         <?php endwhile; ?>
+
+        <?php if ($page >= 2): ?>
+            <a href="index.php?page=<?php print($page - 1); ?>"><?php print($page - 1); ?>ページ目へ</a>
+        <?php endif; ?>
+        <?php
+        $counts = $db->query('SELECT COUNT(*) as cnt FROM memos');
+        $count = $counts->fetch();
+        $max_page = ceil($count['cnt'] / 5);
+        if ($page < $max_page):
+        ?>
+            <a href="index.php?page=<?php print($page + 1); ?>"><?php print($page + 1); ?>ページ目へ</a>
+        <?php endif; ?>
+
     </article>
 </body>
 
