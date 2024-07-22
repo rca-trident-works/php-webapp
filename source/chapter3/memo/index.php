6afa1685329f6ac6d4b01163177842edfe800d36
ろむねこ <nem@nem.x0.to>
2024-07-22 14:46:45 +0900

    wip

--- source/chapter3/memo/index.php
+++ source/chapter3/memo/index.php
@@ -7,6 +7,13 @@
             <?php
                 $memos = $db->prepare('SELECT * FROM memos ORDER BY id DESC');
             ?>
+            <article>
+                <?php while ($memo = $memos->fetch()): ?>
+                    <p><a href="#"><?php print(mb_substr($memo['memo'], 0, 50)); ?></a></p>
+                    <time><?php print($memo['created_at']); ?></time>
+                <hr>
+                <?php endwhile; ?>
+            </article>
         </pre>
     </body>
 </html>
