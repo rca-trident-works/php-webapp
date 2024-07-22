d598f852ade5938904f2d79b9fac50f03b4817d1
ろむねこ <nem@nem.x0.to>
2024-07-22 14:54:54 +0900

    fix

--- source/chapter3/memo/memo.php
+++ source/chapter3/memo/memo.php
@@ -1,13 +1,18 @@
 <?php require('./dbconnect.php'); ?>
 <!DOCTYPE html>
 <html lang="ja">
-    <body>
-        <h2>Practice</h2>
-        <pre>
+
+<body>
+    <h2>Practice</h2>
+    <pre>
             <?php
-                $memos = $db->prepare('SELECT * FROM memos WHERE id=?');
-                $memos->execute(array($_REQUEST['id']));
-                $memo = $memos->fetch();
+            $memos = $db->prepare('SELECT * FROM memos WHERE id=?');
+            $memos->execute(array($_REQUEST['id']));
+            $memo = $memos->fetch();
             ?>
         </pre>
-    </body>
+
+    <article>
+        <pre><?php print($memo['memo']); ?></pre>
+    </article>
+</body>
