e4842fedc690aba50e507395fa1fa48266a1fdc3
ろむねこ <nem@nem.x0.to>
2024-07-22 14:47:00 +0900

    indent

--- source/chapter3/memo/index.php
+++ source/chapter3/memo/index.php
@@ -1,20 +1,19 @@
 <?php require('./dbconnect.php'); ?>
 <!DOCTYPE html>
 <html lang="ja">
-    <body>
-        <h2>Practice</h2>
-        <pre>
-            <?php
-                $memos = $db->prepare('SELECT * FROM memos ORDER BY id DESC');
-            ?>
-            <article>
-                <?php while ($memo = $memos->fetch()): ?>
-                    <p><a href="#"><?php print(mb_substr($memo['memo'], 0, 50)); ?></a></p>
-                    <time><?php print($memo['created_at']); ?></time>
-                <hr>
-                <?php endwhile; ?>
-            </article>
-        </pre>
-    </body>
-</html>
 
+<body>
+    <h2>Practice</h2>
+    <?php
+    $memos = $db->prepare('SELECT * FROM memos ORDER BY id DESC');
+    ?>
+    <article>
+        <?php while ($memo = $memos->fetch()): ?>
+            <p><a href="#"><?php print(mb_substr($memo['memo'], 0, 50)); ?></a></p>
+            <time><?php print($memo['created_at']); ?></time>
+            <hr>
+        <?php endwhile; ?>
+    </article>
+</body>
+
+</html>
