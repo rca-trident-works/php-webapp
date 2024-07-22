cd3df9a20fbf3004f9e077a103e10f4f93a484ba
ろむねこ <nem@nem.x0.to>
2024-07-22 15:13:14 +0900

    fix indent

--- source/chapter3/memo/input_do.php
+++ source/chapter3/memo/input_do.php
@@ -1,15 +1,16 @@
 <?php require('./dbconnect.php'); ?>
 <!DOCTYPE html>
 <html lang="ja">
-    <body>
-        <h2>Practice</h2>
-        <pre>
+
+<body>
+    <h2>Practice</h2>
+    <pre>
             <?php
-                $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
-                $statement -> execute(array($_POST['memo']));
-                echo 'メモが登録されました';
+            $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
+            $statement->execute(array($_POST['memo']));
+            echo 'メモが登録されました';
             ?>
         </pre>
-    </body>
-</html>
+</body>
 
+</html>
